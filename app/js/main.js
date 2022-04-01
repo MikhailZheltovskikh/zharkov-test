8c770d49c2e5ff1bf912e927d3e2b4366543e4e0
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2022-04-01 12:21:43 +0500

    added section facilities

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
-$('.slider__inner').slick({   
+$('.slider__inner').slick({
    arrows: false,
-   fade:true,
+   fade: true,
    dots: true,
+
+});
+
+$('.facilities__slider-inner').slick({
+   arrows: false,
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   arrows: true,
+   nextArrow: '<button class="slick-next slick-next"><img src="images/facilities-slider/slide-next.svg" alt="next arrow"></button>', 
+    prevArrow: '<button class="slick-prev slick-arrow"><img src="images/facilities-slider/slide-prev.svg" alt="next arrow"></button>',  
 
 });
