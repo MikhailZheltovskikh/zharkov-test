47765b406462486ad75b5b308a859cee8dfdd8b6
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2022-04-04 14:51:12 +0500

    added section + hover effect

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,19 @@ $('.facilities__slider-inner').slick({
    slidesToShow: 4,
    slidesToScroll: 1,
    arrows: true,
-   nextArrow: '<button class="slick-next slick-next"><img src="images/facilities-slider/slide-next.svg" alt="next arrow"></button>', 
-    prevArrow: '<button class="slick-prev slick-arrow"><img src="images/facilities-slider/slide-prev.svg" alt="next arrow"></button>',  
+   nextArrow: '<button class="slick-next slick-next"><img src="images/facilities-slider/slide-next.svg" alt="next arrow"></button>',
+   prevArrow: '<button class="slick-prev slick-arrow"><img src="images/facilities-slider/slide-prev.svg" alt="next arrow"></button>',
 
+});
+
+$('.photo-product__item-img').on('click', function () {
+   if ($('.photo-product__item-icon').hasClass('none'),
+      $('.photo-product__item-text').hasClass('active')) {
+      $('.photo-product__item-icon').removeClass('none'),
+      $('.photo-product__item-text').removeClass('active');
+   }
+   else {
+      $('.photo-product__item-icon').addClass('none'),
+      $('.photo-product__item-text').addClass('active');
+   }
 });
